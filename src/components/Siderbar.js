dbc9869237a4e08b5e581665c4686e167d1aad5c
unknown <andrelopessfla@gmail.com>
2020-05-28 02:23:46 -0300

    criado o componente Siderbar

---
+++ src/components/Siderbar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+
+export default class Siderbar extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            modules:[
+                {
+                    id:1,
+                    title:"Iniciando com React",
+                    lessons: [
+                        {id: 1 , title: "Primeira aula"},
+                        {id: 2 , title: "segunda aula"}
+                    ],
+                },
+                {
+                    id:2,
+                    title: "Aprendendo Redux",
+                    lessons:[
+                        {id: 3, title: "Terçeira aula"},
+                        {id: 4, title: "Quarta aula"}
+                    ]
+
+                }
+            ]
+        }
+    }
+    render() {
+        const {modules} = this.state;
+        return (
+           <aside>
+           {
+               modules.map(module =>(
+                   <div  key={module.id}>
+                        <strong>
+                        {module.title}
+                        </strong>
+                        <ul>
+                            {module.lessons.map(lesson => (
+
+                            <li key={lesson.id}>{lesson.title}</li> 
+                            ))}
+                        </ul>
+                   </div>
+               ))
+           }
+           </aside>
+        );
+    }
+}
