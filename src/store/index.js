a6c5c7dc294006c099c8959ae3bf7f8d019c1d9f
unknown <andrelopessfla@gmail.com>
2020-05-28 22:56:26 -0300

    criado constante inicial para inicia o state do reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,34 @@
 import {createStore} from 'redux';
 
-function reducer(){
-    return {
-        activeLesson:null,
-        activeModules:null,
-        modules: [
-            
-                {
-                    id:1,
-                    title:"Iniciando com React",
-                    lessons: [
-                        {id: 1 , title: "Primeira aula"},
-                        {id: 2 , title: "segunda aula"}
-                    ],
-                },
-                {
-                    id:2,
-                    title: "Aprendendo Redux",
-                    lessons:[
-                        {id: 3, title: "Terçeira aula"},
-                        {id: 4, title: "Quarta aula"}
-                    ]
-
-                }
+const INITIAL_STATE ={
+    activeLesson:null,
+    activeModules:null,
+    modules: [
         
-        ]
-    };
+            {
+                id:1,
+                title:"Iniciando com React",
+                lessons: [
+                    {id: 1 , title: "Primeira aula"},
+                    {id: 2 , title: "segunda aula"}
+                ],
+            },
+            {
+                id:2,
+                title: "Aprendendo Redux",
+                lessons:[
+                    {id: 3, title: "Terçeira aula"},
+                    {id: 4, title: "Quarta aula"}
+                ]
+
+            }
+    
+    ]
+
+}
+
+function reducer(state = INITIAL_STATE , action){
+    return state ;
 
 }
 
