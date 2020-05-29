4544828e39b9f934d3f7c1c3b9a1a48e0d09c39a
unknown <andrelopessfla@gmail.com>
2020-05-28 21:52:41 -0300

    criado o componente video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Siderbar from './components/Siderbar';
+import Video from './components/Video';
 
 export default class App extends Component {
     render() {
         return (
             <div>
+                <Video />
                 <Siderbar />
             </div>
         )
---
+++ src/components/Video.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Video extends Component {
+    render() {
+        return (
+            <div>
+                <strong>Módulo x</strong> 
+                <span>Aula x</span>
+            </div>
+        )
+    }
+}
+
