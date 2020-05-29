c6f69ae3156957d7ac07463f3c8ac06d7876a036
unknown <andrelopessfla@gmail.com>
2020-05-28 22:03:05 -0300

    criado store para armazenar o estado inicial usando o redux

---
+++ src/store/index.js
@@ -0,0 +1,28 @@
+import {createStore} from 'redux';
+
+function reducer(){
+    return [
+        {
+            id:1,
+            title:"Iniciando com React",
+            lessons: [
+                {id: 1 , title: "Primeira aula"},
+                {id: 2 , title: "segunda aula"}
+            ],
+        },
+        {
+            id:2,
+            title: "Aprendendo Redux",
+            lessons:[
+                {id: 3, title: "Terçeira aula"},
+                {id: 4, title: "Quarta aula"}
+            ]
+
+        }
+    ];
+
+}
+
+const store  = createStore(reducer);
+
+export default store;
